df4db7b21cc210d1872d250156b8ed71dee1a50f
partizanes <partizanes1@gmail.com>
2018-03-20 10:37:53 +0300

    [5] Implement code execution time logging.

---
+++ ChangeApiBackup_v2/ChangeApiBackup_v2.py
@@ -0,0 +1,30 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+# by Part!zanes 2018
+
+from datetime import datetime
+from log import mainLog
+
+# DEBUG TIMER START
+startTime = datetime.now()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# DEBUG TIMER END
+mainLog.info(datetime.now() - startTime)

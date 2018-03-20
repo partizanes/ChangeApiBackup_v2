e0970458052483b18d1fa0f5e0b26be29671adf2
partizanes <partizanes1@gmail.com>
2018-03-20 15:57:35 +0300

    [13] Update const, add days variable.

--- ChangeApiBackup_v2/const.py
+++ ChangeApiBackup_v2/const.py
@@ -7,3 +7,16 @@ SSH_DIST      = 'pathToBackupDir'
 EMAIL_FROM    = 'user@server'
 MAIL_TO       = ['user@server', 'user@server']
 SMTP_SERVER   = 'localhost'
+
+
+# 1 - Full copy (rsync)
+# 0 - Only changes (cloudlinux changeApi)
+days = {
+    'Monday'    : 1,
+    'Tuesday'   : 0,
+    'Wednesday' : 0,
+    'Thursday'  : 1,
+    'Friday'    : 0,
+    'Saturday'  : 0,
+    'Sunday'    : 0
+    }

0c9e650cf683fd23619b7921cc0bcd5516e8b1a9
partizanes <partizanes1@gmail.com>
2018-03-22 16:09:38 +0300

    [21] [external] change logic in run over ssh.

--- ChangeApiBackup_v2/external.py
+++ ChangeApiBackup_v2/external.py
@@ -2,28 +2,34 @@
 # by Part!zanes 2018
 
 from log import mainLog
-from ssh import runOverSSH
+from ssh import runRemoteSshWithShell
 from const import SSH_DIST, REMOTE_SERVER
 from internal import getCurrentDate, getLastDate
 
 
 # Создает на удаленном сервере директорию с текущим днем
 def createCurrentBackupDir():
-    return runOverSSH('mkdir {0}/{1}'.format(SSH_DIST, getCurrentDate()))
+    mainLog.info("[createCurrentBackupDir] Создаем папку резервного копирования с текущей датой.")
+    return runRemoteSshWithShell('mkdir {0}/{1}'.format(SSH_DIST, getCurrentDate()))
 
 # Проверяет существование предыдущей директории резервной копии
 def lastBackupExits(username):
-    return runOverSSH('stat {0}/{1}/{2}'.format(SSH_DIST, getLastDate(), username))
+    return runRemoteSshWithShell('stat {0}/{1}/{2}'.format(SSH_DIST, getLastDate(), username))
 
 ## Проверяет существование текущий директории резервного копирования
 def currentBackupExits(username):
-    return runOverSSH('stat {0}/{1}/{2}'.format(SSH_DIST, getCurrentDate(), username))
+    return runRemoteSshWithShell('stat {0}/{1}/{2}'.format(SSH_DIST, getCurrentDate(), username))
 
 # Производит копирование на удаленном сервере предыдущего дня в текущий с использованием хардлинков
 # при существовании предыдущей копии и отсутствием текущей
 # в случае наличия текущей копии возвращает False без действий
 def createHardlinkCopy(username):
+    #mainLog.error("[createHardlinkCopy] lastBackupExits: {0} currentBackupExits: {1} total: {2}".format(lastBackupExits(username), currentBackupExits(username), (lastBackupExits(username) and not currentBackupExits(username))))
+
     if(lastBackupExits(username) and not currentBackupExits(username)):
-        return runOverSSH('cp -nlar {0}/{1}/{3} {0}/{2}/')
+        mainLog.info("[createHardlinkCopy][{0}] Создание hardlink копии over ssh".format(username))
+        return runRemoteSshWithShell('cp -nlar {0}/{1}/{3} {0}/{2}/'.format(SSH_DIST, getLastDate(), getCurrentDate(), username, REMOTE_SERVER))
+
     elif(currentBackupExits(username)):
+        mainLog.error("POINT FALSE")
         return False

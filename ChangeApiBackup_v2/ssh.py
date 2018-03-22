c978749d8bb513e09d197f01a3dff163afcdb05d
partizanes <partizanes1@gmail.com>
2018-03-22 15:59:47 +0300

    [16] update runRemoteSshWithShell : use subprocess.

--- ChangeApiBackup_v2/ssh.py
+++ ChangeApiBackup_v2/ssh.py
@@ -1,20 +1,24 @@
 # -*- coding: utf-8 -*-
 # by Part!zanes 2018
 
+import subprocess
+
 from log import mainLog
 from const import REMOTE_SERVER
 
-def runOverSSH(subcmd):
+def runRemoteSshWithShell(subcmd):
     """ Runs the subcmd over ssh, you must ensure that all special characters are
         escaped in the subcommand. Return bool result of execution.
     """
 
-    command = "ssh {REMOTE_SERVER} '{subcmd}'".format(REMOTE_SERVER=REMOTE_SERVER, subcmd=subcmd)
-    answer  = os.system(command)
+    cmd = "ssh {REMOTE_SERVER} '{subcmd}'".format(REMOTE_SERVER=REMOTE_SERVER, subcmd=subcmd)
 
-    mainLog.debug("[runOverSSH][command] {0} [answer] {1}".format(command, answer))
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    out, err = p.communicate()
 
-    if(answer == 0):
+    #mainLog.debug("[runRemoteSshWithShell] {0}".format({'success': True if p.returncode == 0 else False , 'out': out.decode("utf-8"), 'err': err.decode("utf-8"), 'cmd': cmd}))
+
+    if(p.returncode == 0):
         return True
-
+    
     return False

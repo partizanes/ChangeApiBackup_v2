11a35a2e4b275d6ab62a5bfcbcc41711f9702f1c
partizanes <partizanes1@gmail.com>
2018-04-02 11:16:40 +0300

    [46] update log filename format to name_date

--- ChangeApiBackup_v2/log.py
+++ ChangeApiBackup_v2/log.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 # by Part!zanes 2018
 
-import os, logzero
+import os, sys, logzero
 from logzero import setup_logger
+from internal import getCurrentDate
 
-if not os.path.exists('logs'):
-    os.makedirs('logs')
+path = "{0}/logs".format(os.path.dirname(os.path.realpath(sys.argv[0])))
+
+if not os.path.exists(path):
+    os.makedirs(path)
 
 logFormatter = logzero.LogFormatter(fmt=u'%(color)s[%(asctime)s][%(levelname)s] %(message)s', datefmt=u'%Y-%m-%d %H:%M:%S')
-mainLog = setup_logger(name="mainLog", logfile="logs/mainlog.log", formatter=logFormatter)
+mainLog = setup_logger(name="mainLog", logfile="{0}/mainlog_{1}.log".format(path, getCurrentDate().replace('-','_')), formatter=logFormatter)

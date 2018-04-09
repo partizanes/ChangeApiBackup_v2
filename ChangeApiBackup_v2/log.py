c55c434e01199868c92ae6d3027bfed74504bbc9
partizanes <partizanes1@gmail.com>
2018-04-09 12:56:25 +0300

    [50] fix path to log file for cron task.

--- ChangeApiBackup_v2/log.py
+++ ChangeApiBackup_v2/log.py
@@ -2,13 +2,14 @@
 # by Part!zanes 2018
 
 import os, sys, logzero
+from datetime import datetime
 from logzero import setup_logger
-from internal import getCurrentDate
 
 path = "{0}/logs".format(os.path.dirname(os.path.realpath(sys.argv[0])))
+pathToLogName = "{0}/mainlog_{1}.log".format(path, str(datetime.today()).split()[0].replace('-','_'))
 
 if not os.path.exists(path):
     os.makedirs(path)
 
 logFormatter = logzero.LogFormatter(fmt=u'%(color)s[%(asctime)s][%(levelname)s] %(message)s', datefmt=u'%Y-%m-%d %H:%M:%S')
-mainLog = setup_logger(name="mainLog", logfile="{0}/mainlog_{1}.log".format(path, getCurrentDate().replace('-','_')), formatter=logFormatter)
+mainLog = setup_logger(name="mainLog", logfile=pathToLogName, formatter=logFormatter)
